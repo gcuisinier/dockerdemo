321d117f08324693ea29333cde49bb0a418b51c9
Gildas Cuisinier <hikage@hikage.be>
2018-12-13 14:38:12 +0100

    init

---
+++ src/main/java/com/bil/dinorider/dockerdemi/DockerDemiApplication.java
@@ -0,0 +1,14 @@
+package com.bil.dinorider.dockerdemi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemiApplication.class, args);
+	}
+
+}
+
